eedf4d692ff3b32ffa38385861b43656c5d68529
analili6 <82476121+analili6@users.noreply.github.com>
2023-05-09 14:43:33 -0600

    Add files via upload

---
+++ holamundolili.php
@@ -0,0 +1,10 @@
+<?php
+	#Todo mi codigo es PHP
+	$nombre = "Lili";
+	$edad = 22;
+	$ahorro =525.50;
+
+	echo "Hola mundo desde php";
+	echo "<br>Programa creado por $nombre";
+	echo "<br>Mi ahorro es de $ahorro";
+?>
